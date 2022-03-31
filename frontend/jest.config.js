c5619bc058ec5a64025f2ec620d80048fc6bae5a
Simon Hofbauer <simon.hofbauer@tuwien.ac.at>
2022-03-31 15:51:30 +0000

    Feature/#22 create ci cd pipeline

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -6,10 +6,17 @@ module.exports = {
   roots: ['<rootDir>/src/'],
   testMatch: ['**/+(*.)+(spec).+(ts)'],
   setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
-  collectCoverage: true,
-  coverageReporters: ['html'],
+  coverageReporters: ['text-summary'],
   coverageDirectory: 'coverage/my-app',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
     prefix: '<rootDir>/'
-  })
+  }),
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
