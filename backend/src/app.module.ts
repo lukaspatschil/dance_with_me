73084e1a9abb8533e2b2554ea3b78d616d31ba20
Lukas Spatschil <lukas@spatschil.at>
2022-04-04 17:09:28 +0000

    #31 Add winston logger

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,37 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PaymentModule } from './payment/payment.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import winston from 'winston';
+import 'winston-daily-rotate-file';
 
 @Module({
-  imports: [PaymentModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '../.develop.env' }),
+    WinstonModule.forRootAsync({
+      imports: [ConfigModule.forRoot({ envFilePath: '../.develop.env' })],
+      useFactory: async (configService: ConfigService) => ({
+        transports: [
+          new winston.transports.Console(),
+          new winston.transports.DailyRotateFile({
+            filename: `${configService.get('LOG_FILE_NAME', 'log')}-%DATE%.log`,
+            dirname: configService.get('LOG_FILE_PATH', './logs'),
+            datePattern: 'YYYY-MM-DD',
+            zippedArchive: true,
+          }),
+        ],
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.json(),
+        ),
+      }),
+      inject: [ConfigService],
+    }),
+    PaymentModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Logger, ConfigModule],
 })
 export class AppModule {}
