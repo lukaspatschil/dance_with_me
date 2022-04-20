6e3f87e61af8d6415f6c15e20cd81b047066d406
Toni Panzenböck <e1527068@student.tuwien.ac.at>
2022-04-20 15:17:51 +0000

    #66 Add role and authUserId to usershema and create role enum

--- backend/src/core/schema/user.schema.ts
+++ backend/src/core/schema/user.schema.ts
@@ -1,8 +1,15 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { RoleEnum } from './enum/role.enum';
 
 @Schema()
 export class UserDocument extends Document {
+  @Prop({ required: true, unique: true })
+  authUserId!: string;
+
+  @Prop({ required: true })
+  role!: RoleEnum;
+
   @Prop({ required: true })
   displayName!: string;
 
