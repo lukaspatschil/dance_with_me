ab1ebf518229a17797b4ceb0ac897e2153237c56
Simon Hofbauer <simon.hofbauer@tuwien.ac.at>
2022-06-15 19:23:45 +0200

    #168 Implement landing page

--- frontend/src/test/components/landing/landing.component.spec.ts
+++ frontend/src/test/components/landing/landing.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingComponent } from '../../../app/components/landing/landing.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -8,6 +10,7 @@ describe('LandingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, TranslateModule.forRoot()],
       declarations: [ LandingComponent ]
     })
       .compileComponents();
