68c1af29d6e358739752a8d6b6f6d1510bc72f76
Alicia Schwabenbauer <alicia.schwabenbauer@tuwien.ac.at>
2022-06-18 08:20:02 +0200

    changed frontend port

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   baseUrl: 'http://localhost:3000',
   loginUrl: 'loginSite',
   loginCallback: 'login',
-  frontendUrl: 'http://localhost:8080',
+  frontendUrl: 'http://localhost:4200',
   stripeKey: 'pk_test_51L3dZhEJvqr53XbYFT4tBqnKVwurESDFr5TaFPtvbOP6wNh92MOM4NJphxidVvqoGPpqujHz3HHXjrQAkCMOuIup008OuAUonu',
   meiliSearchUrl: 'http://localhost:7700',
   meiliSearchApiKey: 'masterKey'
